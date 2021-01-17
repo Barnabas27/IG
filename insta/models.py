9c1526536cb0b9934c7a9fd851ee4f62cf3d4102
Barnabas27 <bnyambura032@gmail.com>
2021-01-17 15:41:40 +0300

    add functions to delete,save,get and fetch

--- insta/models.py
+++ insta/models.py
@@ -33,11 +33,68 @@ class Profile(models.Model):
         return cls.objects.filter(user__username__icontains = user).all()
         
 class Picture(models.Model):
-    image = models.ImageField(upload_to = 'post/',blank = False, null = True)
+    image = models.ImageField(upload_to = 'images/',blank = False, null = True)
     image_name = models.CharField(max_length = 100)
     image_caption = models.CharField(max_length = 100)
     profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
     likes = models.IntegerField(default=0)
     comments = models.CharField(max_length = 1500)
     
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+        
+    @classmethod
+    def update_caption(self,caption):
+        updated_cap = cls.objects.filter(id = id).update(caption = caption)
+        return updated_cap
     
+    @classmethod
+    def get_all_images(cls):
+        image = cls.objects.all()
+        return image
+    
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id= id).all()
+        return image
+    
+    @classmethod
+    def search_by_profile(cls,name):
+        profile = Profile.objects.filter(user__name__icontains = name)
+        return profile
+    
+    @classmethod
+    def get_one_image(cls,name):
+        image = cls.objects.get(pk = id)
+        return image
+    
+    def __str__(self):
+        return self.image_name
+    
+    class Meta:
+        ordering = ['-date_uploaded']
+    
+class Comments(models.Model):
+    comment = models.CharField(max_length = 50, blank=True)
+    posted = models.DateTimeField(auto_now_add=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+        
+        
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(image__id=id)
+        return comments
+
+
+    def __str__(self):
+        return self.comment
