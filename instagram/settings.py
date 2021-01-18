16330333b5e7af438f75c2a0581c31a084b9fe38
Barnabas27 <bnyambura032@gmail.com>
2021-01-18 21:28:22 +0300

    Deployment to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -89,13 +89,17 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 # development
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': 'instagram',
-           'USER': 'moringa',
-           'PASSWORD': '12345',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
            'PORT': '',
        }
